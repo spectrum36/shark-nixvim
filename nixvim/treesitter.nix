21a8e29e6a18ae0f3d19f768b9d0245c439f0b80
spectrum36 <aidanrm36@gmail.com>
2026-09-23 08:05:41 -0500

    removed treesitter-context, it was annoying me

--- nixvim/treesitter.nix
+++ nixvim/treesitter.nix
@@ -2,20 +2,13 @@
   plugins = {
     treesitter = {
       enable = true;
-      #autoload = true;
       highlight.enable = true;
       indent.enable = true;
       nixvimInjections = true;
     };
 
-    treesitter-context = {
-      enable = true;
-      #autoload = true;
-    };
-
     treesitter-textobjects = {
       enable = true;
-      #autoload = true;
       settings = {
         enable = true;
         lookahead = true;
