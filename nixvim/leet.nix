940fd32b9db2bb9bc313c627e7429471712fddc7
spectrum36 <aidanrm36@gmail.com>
2026-09-24 16:58:18 -0500

    leet

--- nixvim/leet.nix
+++ nixvim/leet.nix
@@ -1,10 +1,9 @@
-{ lib, ...  }: {
+{
   plugins.leetcode = {
     enable = true;
     settings = {
       lang = "python3";
       storage = "~/Projects/leet";
-      cache = lib.nixvim.mkRaw "vim.fn.stdpath('cache') .. '/leetcode'";
     };
   };
 }
